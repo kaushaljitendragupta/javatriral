037f03353a7a0874d5164d804b6c926f5066691c
kaushaljitendra <kaushalgupta1608@gmail.com>
2024-02-12 08:08:25 +0530

    home work is start now

---
+++ hw1.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+
+
+/**
+ * Innerhw1
+ */
+ class ac  {
+int brand ;
+String collor;
+String name;
+int cost;
+void blowair(){
+System.out.println("air is blow ");
+}
+void start(){
+System.out.println("ac start ho gaya ");
+}
+void colleviormment(){
+    System.out.println(" ab maja aaya envirnment coll ho gya ");
+} 
+}
+public class hw1 {
+public static void main(String[] args) {
+
+ac bujaj =new ac();
+bujaj.blowair();
+ac tata =new ac();
+tata.colleviormment();
+}
+    
+}
